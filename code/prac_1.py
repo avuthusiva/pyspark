4950b8e6988b6a8a1dbc4e2f1b6bca9a7c161ebc
AVUTHU SIVA VARDHANA REDDY <53142715+avuthusiva@users.noreply.github.com>
2025-10-28 01:07:17 +0000

    Pyspark code on github

---
+++ code/prac_1.py
@@ -0,0 +1,14 @@
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+spark = SparkSession.builder.appName("Pyspark Application").config("spark.jars","postgresql-42.7.7.jar").getOrCreate()
+url = "jdbc:postgresql://localhost:5432/siva_db"
+properties = {
+    "user": "siva",
+    "password": "postgres",
+    "driver": "org.postgresql.Driver"
+}
+df = spark.read.jdbc(url=url, table="users", properties=properties)
+df.show()
